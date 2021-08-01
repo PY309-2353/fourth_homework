6450e8145cd09c2fd35dba44fb56112070e42ade
kirillvkolkov <86800844+kirillvkolkov@users.noreply.github.com>
2021-08-01 13:59:15 +0300

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,2 +1,18 @@
+# coding=utf-8
 left_list_input = input('Введите первый список ')
 right_list_input = input('Введите второй список ')
+
+def f(list_1, list_2):
+    left_side_counted = len(list_1)
+    right_side_counted = len(list_2)
+
+    if left_side_counted == right_side_counted:
+        total = []
+        for i in range(0, left_side_counted):
+            total.append(left_list_input[i] * right_list_input[i])
+        return total
+    else:
+        return []
+
+
+print(f(left_list_input, right_list_input))
